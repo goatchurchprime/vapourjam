e676c6dafd327ef0eb439ce0820ac974bd1f5cdc
Julian Todd <julian@goatchurch.org.uk>
2023-12-03 13:22:46 +0000

    first intro and level done

--- rooms/roompolyshape.gd
+++ rooms/roompolyshape.gd
@@ -1,9 +1,11 @@
 extends StaticBody3D
 
 
+@export var collisionmeshname = "octogonroom/room_8gon_003"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var facets = $room_poly/room_8gon.mesh.get_faces()
-	facets = $room_poly.transform*$room_poly/room_8gon.transform*facets
+	var collisionmesh = get_node("octogonroom/room_8gon_003")
+	var facets = collisionmesh.mesh.get_faces()
+	facets = global_transform.inverse()*collisionmesh.global_transform*facets
 	$CollisionShape3D.shape.set_faces(facets)
 
