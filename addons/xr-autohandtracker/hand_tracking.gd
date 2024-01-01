9331c4cc058fc7ea950f68f3455d0a2deb0ac729
Julian Todd <julian@goatchurch.org.uk>
2024-01-01 18:42:43 +0000

    auto hand tracking draws the skeleton

--- addons/xr-autohandtracker/hand_tracking.gd
+++ addons/xr-autohandtracker/hand_tracking.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @export var applymiddlefingerfix : bool = true
 @export var applyscaling : bool = true
+@export var coiincidewristorknuckle : bool = true
 
 var xr_interface : OpenXRInterface
 var xrorigin : XROrigin3D
@@ -61,7 +62,6 @@ func extractrestfingerbones():
 			else:
 				assert (f == "Thumb" and b == "Intermediate")
 
-
 func findxrnodes():
 	var nd = self
 	while nd != null:
@@ -100,6 +100,7 @@ func _ready():
 		xr_interface = XRServer.find_interface("OpenXR")
 		set_process(xr_interface != null)
 		extractrestfingerbones()
+		createhandskeleton()
 	else:
 		set_process(false)
 
@@ -142,7 +143,11 @@ func calchandnodetransform(oxrjps, xrt):
 	var m2g1g3 = leftrestreltransform.origin - rightrestreltransform.origin
 	var hnbasis = rotationtoalignB(hstw.basis*m2g1.origin, middleknuckle - wristorigin, 
 							hstw.basis*m2g1g3, leftknuckle - rightknuckle)
+
 	var hnorigin = wristorigin - hnbasis*hstw.origin
+	if not coiincidewristorknuckle:
+		hnorigin = middleknuckle - hnbasis*(hstw*middlerestreltransform).origin
+
 
 	return Transform3D(hnbasis, hnorigin)
 		
@@ -184,6 +189,7 @@ func _process(delta):
 		handnode.top_level = handtrackingactive
 		handnode.get_node("AnimationTree").active = not handtrackingactive
 		print("setting hand "+str(hand)+" active: ", handtrackingactive)
+		$VisibleHandTrackSkeleton.visible = handtrackingactive
 	if handtrackingactive:
 		var oxrjps = getoxrjointpositions()
 		var xrt = xrorigin.global_transform
@@ -191,8 +197,66 @@ func _process(delta):
 		var fingerbonetransformsOut = calcboneposes(oxrjps, handnodetransform, xrt)
 		handnode.transform = handnodetransform
 		copyouttransformstoskel(fingerbonetransformsOut)
+		if visible:
+			updatehandskeleton(oxrjps)
 
 	
 const carpallist = [ OpenXRInterface.HAND_JOINT_THUMB_METACARPAL, OpenXRInterface.HAND_JOINT_INDEX_METACARPAL, OpenXRInterface.HAND_JOINT_MIDDLE_METACARPAL, OpenXRInterface.HAND_JOINT_RING_METACARPAL, OpenXRInterface.HAND_JOINT_LITTLE_METACARPAL ]
 
-	
+func createhandskeleton():
+	var axes3dscene = load("res://addons/xr-autohandtracker/axes3d.tscn")
+	var stickscene = load("res://addons/xr-autohandtracker/stick.tscn")
+	for j in range(OpenXRInterface.HAND_JOINT_MAX):
+		var rj = axes3dscene.instantiate()
+		rj.name = "J%d" % j
+		rj.scale = Vector3(0.01, 0.01, 0.01)
+		#rj.get_node("SkinPad").visible = true
+		#rj.get_node("TipPad").visible = hjtips.has(j)
+		#rj.get_node("Sphere").visible = (j > 0)
+		rj.get_node("ArrowX").visible = false
+		rj.get_node("ArrowY").visible = false
+		rj.get_node("ArrowZ").visible = false
+		$VisibleHandTrackSkeleton.add_child(rj)
+
+	for hjstick in hjsticks:
+		for i in range(0, len(hjstick)-1):
+			var rstick = stickscene.instantiate()
+			var j1 = hjstick[i]
+			var j2 = hjstick[i+1]
+			rstick.name = "S%d_%d" % [j1, j2]
+			rstick.scale = Vector3(0.01, 0.01, 0.01)
+			$VisibleHandTrackSkeleton.add_child(rstick)
+			$VisibleHandTrackSkeleton.get_node("J%d" % hjstick[i+1]).get_node("Sphere").visible = (i > 0)
+
+func rotationtoalign(a, b):
+	var axis = a.cross(b).normalized();
+	if (axis.length_squared() != 0):
+		var dot = a.dot(b)/(a.length()*b.length())
+		dot = clamp(dot, -1.0, 1.0)
+		var angle_rads = acos(dot)
+		return Basis(axis, angle_rads)
+	return Basis()
+
+func sticktransform(j1, j2):
+	var b = rotationtoalign(Vector3(0,1,0), j2 - j1)
+	var d = (j2 - j1).length()
+	return Transform3D(b, (j1 + j2)*0.5).scaled_local(Vector3(0.01, d, 0.01))
+
+func updatehandskeleton(oxrjps):
+	var xrt = xrorigin.global_transform
+	for j in range(OpenXRInterface.HAND_JOINT_MAX):
+		$VisibleHandTrackSkeleton.get_node("J%d" % j).global_transform.origin = xrt*oxrjps[j]
+
+	for hjstick in hjsticks:
+		for i in range(0, len(hjstick)-1):
+			var j1 = hjstick[i]
+			var j2 = hjstick[i+1]
+			var rstick = $VisibleHandTrackSkeleton.get_node("S%d_%d" % [j1, j2])
+			rstick.global_transform = sticktransform(xrt*oxrjps[j1], xrt*oxrjps[j2])
+			
+const hjsticks = [ [ OpenXRInterface.HAND_JOINT_WRIST, OpenXRInterface.HAND_JOINT_THUMB_METACARPAL, OpenXRInterface.HAND_JOINT_THUMB_PROXIMAL, OpenXRInterface.HAND_JOINT_THUMB_DISTAL, OpenXRInterface.HAND_JOINT_THUMB_TIP ],
+				 [ OpenXRInterface.HAND_JOINT_WRIST, OpenXRInterface.HAND_JOINT_INDEX_METACARPAL, OpenXRInterface.HAND_JOINT_INDEX_PROXIMAL, OpenXRInterface.HAND_JOINT_INDEX_INTERMEDIATE, OpenXRInterface.HAND_JOINT_INDEX_DISTAL, OpenXRInterface.HAND_JOINT_INDEX_TIP ],
+				 [ OpenXRInterface.HAND_JOINT_WRIST, OpenXRInterface.HAND_JOINT_MIDDLE_METACARPAL, OpenXRInterface.HAND_JOINT_MIDDLE_PROXIMAL, OpenXRInterface.HAND_JOINT_MIDDLE_INTERMEDIATE, OpenXRInterface.HAND_JOINT_MIDDLE_DISTAL, OpenXRInterface.HAND_JOINT_MIDDLE_TIP ],
+				 [ OpenXRInterface.HAND_JOINT_WRIST, OpenXRInterface.HAND_JOINT_RING_METACARPAL, OpenXRInterface.HAND_JOINT_RING_PROXIMAL, OpenXRInterface.HAND_JOINT_RING_INTERMEDIATE, OpenXRInterface.HAND_JOINT_RING_DISTAL, OpenXRInterface.HAND_JOINT_RING_TIP ],
+				 [ OpenXRInterface.HAND_JOINT_WRIST, OpenXRInterface.HAND_JOINT_LITTLE_METACARPAL, OpenXRInterface.HAND_JOINT_LITTLE_PROXIMAL, OpenXRInterface.HAND_JOINT_LITTLE_INTERMEDIATE, OpenXRInterface.HAND_JOINT_LITTLE_DISTAL, OpenXRInterface.HAND_JOINT_LITTLE_TIP ]
+			   ]
